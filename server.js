0f5c6b4e77c3fac54c727c9eae2f40277cf78da2
WallyD <86563710+WallyDD-pixel@users.noreply.github.com>
2024-04-22 09:47:44 +0200

    version api square

--- server.js
+++ server.js
@@ -1,37 +1,50 @@
 const express = require('express');
 const cors = require('cors');
+const { Client, Environment } = require('square');
 const app = express();
 
-app.get('/create-checkout/:itemId', (req, res) => {
-  const squareConnect = require('square-connect');
-  const defaultClient = squareConnect.ApiClient.instance;
+// Utilisez le middleware cors
+app.use(cors());
 
-  // Configurez OAuth2 Access Token pour l'authentification
-  const oauth2 = defaultClient.authentications['oauth2'];
-  oauth2.accessToken = 'EAAAl7tcgKqwP3lUH0hCg1c0utawSbUsRQnE2JBVamyxb8MWWfqqQ9nBwy7V9wpZ';
-
-  const apiInstance = new squareConnect.CheckoutApi();
-
-  const locationId = 'LWZ1NPVNSDA2M';
-  const body = new squareConnect.CreateCheckoutRequest();
-
-  body.idempotency_key = new Date().getTime().toString();
-  body.order = {
-    line_items: [
-      {
-        quantity: '1',
-        catalog_object_id: req.params.itemId // Utilisez l'ID de l'article passé en tant que paramètre de requête
-      }
-    ]
-  };
-
-  apiInstance.createCheckout(locationId, body)
-    .then((data) => {
-      res.json({ checkoutUrl: data.checkout.checkout_page_url });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Erreur lors de la création du checkout');
-    });
+// Créez une nouvelle instance de client
+const client = new Client({
+  environment: Environment.Sandbox, // ou Environment.Production production
+  accessToken: 'EAAAl7tcgKqwP3lUH0hCg1c0utawSbUsRQnE2JBVamyxb8MWWfqqQ9nBwy7V9wpZ', // remplacez par votre jeton d'accès
 });
-app.listen(3000, () => console.log('Server listening on port 3000'));
+
+app.get('/create-payment-link', async (req, res) => {
+  try {
+    // Récupérer l'id de l'abonnement à partir des paramètres de requête
+    const amount = req.query.amount;
+    const title = req.query.title;
+
+    // Vérifier si l'id de l'abonnement est présent
+    if (!amount || !title) {
+      res.status(400).send('Montant ou titre manquant');
+      return;
+    }
+    
+    const response = await client.checkoutApi.createPaymentLink({
+      quickPay: {
+        name: title,
+        priceMoney: {
+          amount: parseInt(amount * 100),
+          currency: 'EUR',
+          
+        },
+        locationId: 'LWZ1NPVNSDA2M'
+      },
+      checkoutOptions: {
+        subscriptionPlanId: ''
+      }
+    });
+
+    // Renvoie l'URL du lien de paiement
+    res.send(response.result.paymentLink.url);
+  } catch(error) {
+    console.log(error);
+    res.status(500).send('Erreur lors de la création du lien de paiement');
+  }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
