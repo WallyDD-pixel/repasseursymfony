0210d7c2ae24e28884489f1c7bc46cc0d907f044
WallyD <86563710+WallyDD-pixel@users.noreply.github.com>
2024-08-08 17:49:39 +0200

    DSD

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Utiliser la clé secrète Stripe depuis les variables d'environnement
 const cors = require('cors'); // Importer le middleware CORS
+const path = require('path'); // Importer path pour gérer les chemins de fichiers
 const app = express();
 const PORT = process.env.PORT || 3000; // Utiliser le port fourni par Render ou 3000 en local
 
-
 // Utiliser le middleware CORS
 app.use(cors());
-
-app.use(express.static('public'));
 app.use(express.json());
 
+// Servir les fichiers statiques depuis le dossier 'public'
+app.use(express.static('public'));
+
+// Servir le fichier 'index.html' situé à la racine lorsqu'un utilisateur accède à la racine '/'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route pour créer une session de paiement avec abonnement
 app.post('/create-checkout-session', async (req, res) => {
   const { amount, description, email, title } = req.body;
   const successUrl = new URL('http://le-repasseur.fr:3000/success.html');
@@ -18,6 +25,7 @@ app.post('/create-checkout-session', async (req, res) => {
   successUrl.searchParams.append('description', description);
   successUrl.searchParams.append('email', email);
   successUrl.searchParams.append('title', title);
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: [
@@ -38,12 +46,14 @@ app.post('/create-checkout-session', async (req, res) => {
     ],
     mode: 'subscription', // Mode d'abonnement
     customer_email: email,
-    success_url: successUrl.toString(), // Utiliser l'URL de redirection avec les paramètres
+    success_url: successUrl.toString(),
     cancel_url: 'http://le-repasseur.fr:3000/cancel.html',
   });
 
   res.json({ id: session.id });
 });
+
+// Route pour créer une session de paiement pour un produit (unique ou abonnement)
 app.post('/create-checkout-session-produits', async (req, res) => {
   const { amount, description, email, title, type } = req.body;
   const successUrl = new URL('http://le-repasseur.fr:3000/success.html');
@@ -68,7 +78,7 @@ app.post('/create-checkout-session-produits', async (req, res) => {
       },
     ],
     customer_email: email,
-    success_url: successUrl.toString(), // Utiliser l'URL de redirection avec les paramètres
+    success_url: successUrl.toString(),
     cancel_url: 'http://le-repasseur.fr:3000/cancel.html',
   };
 
@@ -86,4 +96,5 @@ app.post('/create-checkout-session-produits', async (req, res) => {
   res.json({ id: session.id });
 });
 
+// Lancer le serveur
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
